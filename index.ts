6dd8c4c559064120afb36ada1dea7e2304b46207
EseAlli <essealli.ea@gmail.com>
2022-11-13 08:21:05 +0100

    implemented yearly discount

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
 const range = <HTMLInputElement>document.querySelector(".range");
 const price = <HTMLHeadingElement>document.querySelector("h2");
+const pageview = <HTMLParagraphElement>document.querySelector(".pageviews");
+const toggleBilling = <HTMLInputElement>document.querySelector(".switch input");
+let yearlyDiscount: boolean = toggleBilling.checked;
+let currentPageview: string = "100K";
+range.addEventListener("change", pricing);
 
-console.log(range);
-
-range.addEventListener("change", function () {
-  console.log(range.value);
-  price.firstChild ? (price.firstChild.nodeValue = `$${this.value}.00`) : null;
+toggleBilling.addEventListener("click", function () {
+  this.checked = !yearlyDiscount;
+  yearlyDiscount = this.checked;
 });
+
+function pricing() {
+  let priceRange: any = Number(range.value).toFixed(2);
+  priceRange = !yearlyDiscount ? priceRange : priceRange / 0.25;
+  currentPageview = !yearlyDiscount
+    ? String(Math.ceil(priceRange * 6.25))
+    : String(Math.ceil((priceRange * 6.25) / 0.25));
+  pageview.firstChild
+    ? (pageview.firstChild.nodeValue = `${currentPageview}K `)
+    : null;
+  price.firstChild ? (price.firstChild.nodeValue = `$${priceRange}`) : null;
+}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 "use strict";
 const range = document.querySelector(".range");
 const price = document.querySelector("h2");
-console.log(range);
-range.addEventListener("change", function () {
-    console.log(range.value);
-    price.firstChild ? (price.firstChild.nodeValue = `$${this.value}.00`) : null;
+const pageview = document.querySelector(".pageviews");
+const toggleBilling = document.querySelector(".switch input");
+let yearlyDiscount = toggleBilling.checked;
+let currentPageview = "100K";
+range.addEventListener("change", pricing);
+toggleBilling.addEventListener("click", function () {
+    this.checked = !yearlyDiscount;
+    yearlyDiscount = this.checked;
 });
+function pricing() {
+    let priceRange = Number(range.value).toFixed(2);
+    priceRange = !yearlyDiscount ? priceRange : priceRange / 0.25;
+    currentPageview = !yearlyDiscount
+        ? String(Math.ceil(priceRange * 6.25))
+        : String(Math.ceil((priceRange * 6.25) / 0.25));
+    pageview.firstChild
+        ? (pageview.firstChild.nodeValue = `${currentPageview}K `)
+        : null;
+    price.firstChild ? (price.firstChild.nodeValue = `$${priceRange}`) : null;
+}
